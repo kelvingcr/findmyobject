6d149695e4e2279d7bfc0016e877af6eb788a02d
Kelvin Rodrigues <contato@kelvindev.tk>
2022-06-10 09:05:26 -0300

    Retrofit instance

---
+++ app/src/main/java/br/com/kelvingcr/findmyobject/service/RetrofitClient.kt
@@ -0,0 +1,33 @@
+package br.com.kelvingcr.findmyobject.service
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class RetrofitClient private constructor(){
+
+    companion object {
+
+        private lateinit var retrofit: Retrofit
+        private val baseURL = "https://proxyapp.correios.com.br/v1/sro-rastro/"
+
+       private fun getRetrofitInstance() : Retrofit {
+
+            //Gerencia as comunicação com chamadas http
+            val httpClient = OkHttpClient.Builder()
+
+            if(!Companion::retrofit.isInitialized) {
+                retrofit = Retrofit.Builder()
+                    .baseUrl(baseURL)
+                    .client(httpClient.build())
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build()
+            }
+            return retrofit
+        }
+
+        fun <S> createService(serviceClass: Class<S>) : S {
+            return getRetrofitInstance().create(serviceClass)
+        }
+    }
+}
